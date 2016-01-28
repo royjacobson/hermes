8e23f07f556108649c33eee99b22cde0b32099c6
Roy Jacobson <roi.jacobson1@gmail.com>
2016-01-29 00:23:03 +0200

    אין לי כוח לכתוב מה עשיתי

--- server.py
+++ server.py
@@ -1,4 +1,10 @@
 import eventbrite
+import socketserver
+import json
+
+
+PORT = 425
+##Date format: DD
 
 class Location:
     def __init__(self, name, coords):
@@ -10,10 +16,22 @@ class Query:
         pass
 
 class Event:
-    def __init__(self, info, location, dates, url):
+    def __init__(self, info, location, dates, price, url):
         self.info_text = info
         self.location = location
         self.dates = dates
+        self.price = price
+        self.url = url
+
+class Trip:
+    def __init__(self, dates, price, route, url):
+        """
+        route = (src, dest) which are both Location() instances
+        price in $
+        """
+        self.dates = dates
+        self.price = price
+        self.route = route
         self.url = url
 
 
@@ -28,6 +46,23 @@ def get_trips(origin, target, date):
 
 
 def get_offers(query):
-
-while True:
     pass
+
+
+class MyTCPHandler(socketserver.StreamRequestHandler):
+
+    def handle(self):
+        # self.rfile is a file-like object created by the handler;
+        # we can now use e.g. readline() instead of raw recv() calls
+        query = b''.join(self.rfile.readlines())
+        json_query = json.load(query)
+        print("{} wrote:".format(self.client_address[0]))
+        from pprint import pprint
+        pprint(json_query)
+        # Likewise, self.wfile is a file-like object used to write back
+        # to the client
+
+if __name__ == "__main__":
+    HOST, PORT = "", 4356
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    server.serve_forever()
