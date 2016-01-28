2bfd5fc3fd6fd1bb1548af8eff39de6b96f6ecb6
Lev Petrov <petrov609@gmail.com>
2016-01-29 00:58:00 +0200

    Fixed activity_main.xml

--- HermesAPK/app/src/main/java/com/hermes/hermes/MainActivity.java
+++ HermesAPK/app/src/main/java/com/hermes/hermes/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        
+
 
 
     }
