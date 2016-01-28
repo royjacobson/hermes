70119eda2eb67b53bdae2bda38f976da358d5384
Lev Petrov <petrov609@gmail.com>
2016-01-28 22:38:49 +0200

    ied:   HermesAPK/.idea/misc.xml

--- HermesAPK/app/src/main/java/com/hermes/hermes/Query.java
+++ HermesAPK/app/src/main/java/com/hermes/hermes/Query.java
@@ -1,22 +1,43 @@
 package com.hermes.hermes;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
 
-/**
- * Created by Lev Petrov on 28-Jan-16.
- */
 public class Query {
 
     String json;
+    static final String serverIP = "172.29.104.189";
+    static final int serverPort = 4356;
+    private Context context;
 
-    public Query(String json){
+    public Query(String json, Context context) {
         this.json = json;
+        this.context = context;
     }
 
-    public void sendQuery(){
-        SocketAddress socketAddress =
+    public void sendQuery() {
         Socket socket = new Socket();
-        socket.connect();
+        SocketAddress socketAddress = new InetSocketAddress(serverIP, serverPort);
+        OutputStream outputStream = null;
+        PrintWriter printWriter = null;
+        try {
+            socket = new Socket(serverIP, serverPort);
+            socket.connect(socketAddress);
+            outputStream = socket.getOutputStream();
+            printWriter = new PrintWriter(outputStream);
+            printWriter.print("Lev Petrov");
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(context,"Something went wrong during the socket opening", Toast.LENGTH_LONG)
+                    .show();
+        }
+
     }
 }
