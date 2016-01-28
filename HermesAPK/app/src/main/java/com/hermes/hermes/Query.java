64526035200fe9a5eb80ceb7e407254a64d821f6
Lev Petrov <petrov609@gmail.com>
2016-01-28 20:52:44 +0200

    Minor changes

--- HermesAPK/app/src/main/java/com/hermes/hermes/Query.java
+++ HermesAPK/app/src/main/java/com/hermes/hermes/Query.java
@@ -1,7 +1,22 @@
 package com.hermes.hermes;
 
+import java.net.Socket;
+import java.net.SocketAddress;
+
 /**
  * Created by Lev Petrov on 28-Jan-16.
  */
 public class Query {
+
+    String json;
+
+    public Query(String json){
+        this.json = json;
+    }
+
+    public void sendQuery(){
+        SocketAddress socketAddress =
+        Socket socket = new Socket();
+        socket.connect();
+    }
 }
